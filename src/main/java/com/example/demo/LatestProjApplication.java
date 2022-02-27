31ab4937be48c5fdfdab352271bad2ba9fffcf2a
Shahim Ejaz <shahimejaz@Shahims-MacBook-Pro.local>
2022-02-27 23:47:45 +1100

    Project-management-system-First-commit

---
+++ src/main/java/com/example/demo/LatestProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatestProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LatestProjApplication.class, args);
+	}
+
+}
